4e6d2683d7abf2b625b250cbe53cc0697524610a
Kim youngtae <ytkim@infovine.co.kr>
2024-09-03 16:33:23 +0900

    [UPDATE] 폼 유효성 검사 및 에러 코드 메시지 커스텀

--- app/create-account/action.ts
+++ app/create-account/action.ts
@@ -2,12 +2,21 @@
 import { z } from 'zod';
 
 const usernameSchema = z.string().min(5).max(10);
-const formSchema = z.object({
-  username: z.string().min(5).max(10),
-  email: z.string().email(),
-  password: z.string().min(10),
-  confirm_password: z.string().min(10),
-});
+
+const checkPasswords = ({ password, confirm_password }: { password: string; confirm_password: string }) =>
+  password === confirm_password;
+
+const formSchema = z
+  .object({
+    username: z.string().min(5, '길이가 너무 짧습니다.').max(10, '길이가 너무 깁니다.'),
+    email: z.string().email(),
+    password: z.string().min(10),
+    confirm_password: z.string().min(10),
+  })
+  .refine(checkPasswords, {
+    message: '비밀번호가 일치해야합니다.',
+    path: ['confirm_password'],
+  });
 
 export async function createAccount(prevState: any, formData: FormData) {
   const data = {
